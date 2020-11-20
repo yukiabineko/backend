8cfd15bb43d6dd5699156bafc661a41610031f27
yukiabineko <yukiabineko@live.com>
2020-11-20 20:26:34 +0900

    11/20 rspec user create

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -14,7 +14,21 @@ describe "ユーザーテスト", type: :request  do
       get users_path
       expect(response.status).to eq 200
     end
+  end
+###############################################################
+  describe "ユーザー新規作成テスト" do
+    it "新規作成される" do
+      valid_params = { 
+        name: 'ポチ', 
+        email: 'pochi@example.com',
+        password: '123',
+        password_confirmation: '123'
+      }
+      expect { post users_path, params: valid_params }.to change(User, :count).by(1) #=>ユーザーが増える
+      expect(response.status).to eq 200
+    end
     
   end
+  
   
 end
