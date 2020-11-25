1486e60bf31ff3e89a84f4d200638f6ac7232cd1
yukiabineko <yukiabineko@live.com>
2020-11-25 22:21:15 +0900

    11/25 itemモデル追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,4 +17,22 @@ User.create!(
     password: '123',
     password_confirmation: '123'
 )
+Item.create!(
+    name: '真あじ',
+    price: 120,
+    category: '青魚',
+    info: '大衆魚です。様々な調理に利用できます。'
+)
+Item.create!(
+    name: '真ガレイ',
+    price: 400,
+    category: '白身魚',
+    info: '煮物に最適。'
+)
+Item.create!(
+    name: '帆立貝',
+    price: 150,
+    category: '貝類',
+    info: '風味のある色々な調理に向いてます。'
+)
 
