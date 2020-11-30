fdfbbfa76ae1158efee1afe21c3281fce35e7237
yukiabineko <yukiabineko@live.com>
2020-11-30 17:59:42 +0900

    11/30 order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,4 +3,35 @@ class OrdersController < ApplicationController
     orders = Order.all.rent
     render json: orders
   end
+  
+  def create
+    order = Order.new( order_parameter )
+    if order.save
+      render json: {message: '登録しました。'}
+    else
+      render json: {message: '失敗しました。'}
+    end
+  end
+
+  def update
+    order = Order.find( params[:id] )
+    if order.update_attributes( order_parameter )
+      render json: {message: '登録しました。'}
+    else
+      render json: {message: '失敗しました。'}
+    end
+  end
+
+  def destroy
+    order = Order.find( params[:id] )
+    order.destroy
+    render json: {message: '削除しました。'}
+  end
+
+
+private
+  def order_parameter
+    params.permit( :name, :price, :oder_day, :stock )
+  end
+  
 end
