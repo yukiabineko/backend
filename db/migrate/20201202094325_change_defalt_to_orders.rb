51a640707921505d9d1ef17cfc86275cb4753715
yukiabineko <yukiabineko@live.com>
2020-12-02 19:05:37 +0900

    11/2 fix

--- db/migrate/20201202094325_change_defalt_to_orders.rb
+++ db/migrate/20201202094325_change_defalt_to_orders.rb
@@ -1,9 +1,9 @@
 class ChangeDefaltToOrders < ActiveRecord::Migration[5.2]
   def up
-    change_column :orders, :oder_daye, :date, default:  Date.today + 1
+    change_column :orders, :oder_day, :date, default:  Date.today + 1
   end
 
   def down
-    change_column :orders, :oder_daye, :date
+    change_column :orders, :oder_day, :date
   end
 end
