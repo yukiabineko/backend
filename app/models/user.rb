8a5b751ccdf34300db3a68a9568c54dbb953a5a4
abineko2 <yuki1980426@gmail.com>
2021-01-03 08:00:22 +0900

    1/3 fix session

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ApplicationRecord
 			hash[:name] = self.name
 			hash[:email]= self.email
 			hash[:create] = self.created_at
+			hash[:admin] = self.admin
 
 			array = []
 			self.shoppings.all.each do |shopping|
