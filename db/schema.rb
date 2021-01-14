2700abceefbdce5fda3e96d5a70261f59fffa355
abineko2 <yuki1980426@gmail.com>
2021-01-14 19:50:33 +0900

    1/14 shoppings 受け取り時間追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_02_155502) do
+ActiveRecord::Schema.define(version: 2021_01_14_100232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2021_01_02_155502) do
   create_table "orders", force: :cascade do |t|
     t.string "name"
     t.integer "price"
-    t.date "oder_day", default: "2021-01-10"
+    t.date "oder_day", default: "2021-01-04"
     t.integer "stock"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2021_01_02_155502) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "status", default: 0
+    t.datetime "receiving_time"
     t.index ["user_id"], name: "index_shoppings_on_user_id"
   end
 
