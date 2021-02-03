ac108478d550051893de9980822ef62363b8e95e
植松勇貴 <uematsuyuuki@uematsuyuukinoMacBook-Pro.local>
2021-02-03 12:29:10 +0900

    売り上げデータ

--- app/models/shopping.rb
+++ app/models/shopping.rb
@@ -1,4 +1,11 @@
 class Shopping < ApplicationRecord
   belongs_to :user
+
+
+  def self.sale_data(params)
+    first_day = params.present? ?Date.parse(params).beginning_of_month : Date.today.beginning_of_month
+    lase_day = first_day.end_of_month
+    where(status: 2).where(shopping_date: first_day .. lase_day).order(shopping_date: :ASC)
+  end
   
 end
