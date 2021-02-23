408cb1e9c2bec5a66bd3430c527a4ee5d211ad14
msk-ya <masakiabineko@gmail.com>
2021-02-23 10:32:22 +0900

    fix

--- app/controllers/shopping_phone_controller.rb
+++ app/controllers/shopping_phone_controller.rb
@@ -1,7 +1,6 @@
 class ShoppingPhoneController < ApplicationController
 
   def create
-    debugger
     message = '失敗。'
     order = Order.find_by(name: params[:name])
     order.stock -= params[:num].to_i
@@ -15,7 +14,7 @@ class ShoppingPhoneController < ApplicationController
         num: params[:num],
         process: params[:process],
         shopping_date: (Date.today + 1).strftime('%Y/%m/%d'),
-        receiving_time:  "#{(Date.today + 1).strftime('%Y/%m/%d')} #{param[:time].to_s}"
+        receiving_time:  "#{(Date.today + 1).strftime('%Y/%m/%d')} #{params[:time].to_s}"
         
       )
       message = '登録しました'
