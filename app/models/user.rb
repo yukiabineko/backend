7eb32a28d2e4a8fab1492522f35c8c3d24ad8e2c
植松勇貴 <uematsuyuuki@uematsuyuukinoMacBook-Pro.local>
2021-03-07 01:09:04 +0900

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
 		hash[:id] = self.id
 		hash[:name] = self.name
 		hash[:email]= self.email
-		hash[:tel] = self.tel  if self.tel.present?
+		hash[:tel] = self.tel.present? ?self.tel : ''
 		hash[:create] = self.created_at
 		hash[:admin] = self.admin
 
