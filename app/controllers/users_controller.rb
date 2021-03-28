2b4804004c11fcd48d17452435072a49a14991db
植松勇貴 <uematsuyuuki@uematsuyuukinoMacBook-Pro.local>
2021-03-28 12:01:33 +0900

    3/28登録処理修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,11 +13,18 @@ class UsersController < ApplicationController
 
   def create
     user = User.new( user_parameter )
-    if user.save
-      render json: {message: '登録しました', userData: user.user_history}
+    #employeeパラメーターが付与された場合登録禁止
+    if params[:employee]
+        render json: {message: '登録失敗しました。内容を確認してください'}
 
+    #employeeパラメーターが付与されない場合登録開始
     else
-      render json: {message: '登録失敗しました。内容を確認してください'}
+      if user.save
+        render json: {message: '登録しました', userData: user.user_history}
+
+      else
+        render json: {message: '登録失敗しました。内容を確認してください'}
+      end
     end
   end
 
