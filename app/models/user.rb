d7ec9fe68f58e25f0f4490d5879d09cf431d34af
植松勇貴 <uematsuyuuki@uematsuyuukinoMacBook-Pro.local>
2021-04-05 19:51:06 +0900

    4/5バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -72,7 +72,7 @@ class User < ApplicationRecord
 	#エラーメッセージを加工
 	def errors_message_set
 		str = ''
-		errors_full_messages.each{|err| str += err + '\n'}
+		errors.full_messages.each{|err| str += err + '\n'}
 	    return str
 	end
 
