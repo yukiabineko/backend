c60b8e98b239a3a830ee96209075047e76b7fb26
植松勇貴 <uematsuyuuki@uematsuyuukinoMacBook-Pro.local>
2021-04-15 20:57:50 +0900

    ユーザー編集パス修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::API
   def current_user_check_edit?
     status = false
     user = User.find_by( email: params[ :oldmail ])
-    if user&.authenticate(params[:password])
+    if user&.authenticate(params[:oldpass])
       status = true
     end
     return status
