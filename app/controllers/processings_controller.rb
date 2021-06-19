da660e069d5ac49715f651d1246fb2312d501b36
植松勇貴 <uematsuyuuki@uematsuyuukinoMacBook-Pro.local>
2021-06-19 19:14:47 +0900

    windowsアプリ対応(process)

--- app/controllers/processings_controller.rb
+++ app/controllers/processings_controller.rb
@@ -6,12 +6,26 @@ class ProcessingsController < ApplicationController
   end
 
   def create
-    item = Item.find( params[:id] )
-    params[:data].each do |data|
-      processing = item.processings.new(processing_name: data[:value])
-      processing.save
+    
+     ##送信元がwebアプリの場合
+    if params[:data]
+      item = Item.find( params[:id] )
+      params[:data].each do |data|
+        processing = item.processings.new(processing_name: data[:value])
+        processing.save
+      end
+      render json:{message: '送信しました。'}
+
+    ##送信元がwindowsアプリの場合
+    elsif params[:win_processes]
+      array = windows_string_to_array(params[:win_processes])
+      array.each do |data|
+        processing = item.processings.new(processing_name: data)
+        processing.save
+      end
+      render json:{message: '送信しました。'}
     end
-    render json:{message: '送信しました。'}
+    
   end
 
 
@@ -20,6 +34,14 @@ class ProcessingsController < ApplicationController
     processing.destroy
     render json: {message: '削除しました。'}
   end
-  
+private
+
+  def windows_string_to_array(string)  
+     newArray = []
+     string.split(",").each do |string|
+       string.present? ?newArray<< string : ""
+     end
+     return newArray.uniq
+  end
   
 end
